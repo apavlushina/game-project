5b96724328ff88cbd55101f0b16b5f95161c6053
apavlushina <ssemina7@gmail.com>
2019-11-20 00:55:44 +0100

    fix users table

--- room/router.js
+++ room/router.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const Room = require("./model");
+const User = require("../users/model");
 const auth = require("../auth/middleware");
+const { toData } = require("../auth/jwt");
 
 const { Router } = express;
 
@@ -19,6 +21,33 @@ function roomFactory(stream) {
       stream.send(data);
       response.send(room);
     });
+  });
+
+  router.put("/users/join", auth, async (req, res, next) => {
+    console.log("what is this?", req.body.jwt, req.body);
+    const userId = toData(req.body.jwt).userId;
+    const room = await Room.findOne({ where: { name: req.body.roomName } });
+    const roomId = room.dataValues.id;
+    console.log("IT WORKS", userId, roomId);
+
+    const user = await User.findByPk(userId);
+    // console.log("user test", user);
+    if (!user) {
+      return next("No user found");
+    }
+
+    const updated = await user.update({ roomId });
+
+    const rooms = await Room.findAll({ include: [User] });
+    const action = {
+      type: "ROOMS",
+      payload: rooms
+    };
+    const string = JSON.stringify(action);
+
+    stream.send(string);
+
+    response.send(updated => console.log(updated));
   });
 
   return router;
